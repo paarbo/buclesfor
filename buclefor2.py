7d8eb552da8a4eec37ade662c8c392f85e85a67d
root <root@localhost.localdomain>
2018-02-15 13:22:56 +0100

    first commit

--- buclefor2.py
+++ buclefor2.py
@@ -3,7 +3,7 @@ num = int(input("Introduce un número entero mayor que cero: "))
 if num <=0:
     print("no valido")
 else:
-    print("Los divisores de",num,"son", end=" ")
-    for i in range(1, num + 1):
-        if num % i==0:
-            print(i, end=" ")
+    print("Los divisores de",num,"son...")
+    for n in range(1, num+1):
+        if num % n==0:
+            print(n)
